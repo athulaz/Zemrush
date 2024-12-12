1d24b3fd2f5fb42085ef2f5e38bcd1deb9dcf3da
athulaz <athul8589@gmail.com>
2024-12-12 22:10:42 +0530

    Updated header with design changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
 
-function Header() {
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChartPie } from "@fortawesome/free-solid-svg-icons"; // Replace this with any icon you prefer
+import "./Header.css";
+const Header = () => {
   return (
-    <>
-      <header>Header</header>
-    </>
-  )
-}
+    <header className="header">
+      <div className="header-container">
+        {/* Logo */}
+        <div className="logo">
+          <FontAwesomeIcon icon={faChartPie} className="logo-icon" />
+          <span className="logo-text">SEMRUSH</span>
+        </div>
 
-export default Header
+        {/* Navigation Links */}
+        <nav className="nav">
+          <a href="#features">Features</a>
+          <a href="#pricing">Pricing</a>
+          <a href="#resources">Resources</a>
+          <a href="#company">Company</a>
+          <a href="#app-center">
+            App Center <span className="new-badge">new</span>
+          </a>
+          <a href="#enterprise">Enterprise</a>
+        </nav>
+
+        {/* Language Selector and Buttons */}
+        <div className="actions">
+          <select className="language-selector" defaultValue="EN">
+            <option value="EN">EN</option>
+            <option value="ES">ES</option>
+          </select>
+          <button className="login-btn">Log In</button>
+          <button className="signup-btn">Sign Up</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
