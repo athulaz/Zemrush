9ab2a9e14bd2c6f2d0df138db6a77de49eb293b2
aswanth7356 <aswanth1392@gmail.com>
2024-12-12 20:03:32 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from '../components/Header';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './Home.css'
+
+
+function Home() {
+    return (
+
+        <>
+            <Header />
+            <div className='square'>
+                <div className="circle">
+
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Home
